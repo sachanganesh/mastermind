e452dd5b88ee6a7165a972b3e496a3d0da666aee
Sachandhan Ganesh <me@sachanganesh.com>
2015-04-18 19:40:03 -0700

    Complete Scala game

--- Mastermind.scala
+++ Mastermind.scala
@@ -6,16 +6,18 @@ object Mastermind {
 	val input = new Scanner(System.in)
 	// Max # of guesses a player can make
 	val maxTurns = 10
+	// # of pins and markers for a code
+	val numPins = 4
 
 	// All player guesses
-	val guesses = Array.ofDim[Char](maxTurns, 4)
+	val guesses = Array.ofDim[Char](maxTurns, numPins)
 	// All markers for player guesses
-	val markers = Array.ofDim[Char](maxTurns, 4)
+	val markers = Array.ofDim[Char](maxTurns, numPins)
 
 	// Pin choices
-	val colorChoices = Array('R', 'G', 'B', 'O', 'P', 'Y')
+	val colorChoices = Array('B', 'G', 'O', 'P', 'R', 'Y')
 	// Computer pins selection
-	val computerCode = new Array[Char](4)
+	val computerCode = new Array[Char](numPins)
 
 	// Turn #
 	var turn = 0
@@ -50,21 +52,21 @@ object Mastermind {
 		println("------------")
 		println("It's just you versus the computer.\nMastermind is known as the codebreaker's game, and as you read/play you will see why!\n")
 		println("There are six colored pins.")
-		println("The red pin is represented by `R`")
-		println("The green pin is represented by `G`")
 		println("The blue pin is represented by `B`")
+		println("The green pin is represented by `G`")
 		println("The orange pin is represented by `O`")
 		println("The purple pin is represented by `P`")
+		println("The red pin is represented by `R`")
 		println("The yellow pin is represented by `Y`")
 		println
-		println("The computer will choose a permutation of four colored pins.")
+		println("The computer will choose a permutation of " + numPins + " colored pins.")
 		println
 		println("You will have " + maxTurns + " turns to find the computer's code.")
-		println("Every turn, you will choose a permutation of four pins to match against the computer's choice.")
+		println("Every turn, you will choose a permutation of " + numPins + " pins to match against the computer's choice.")
 		println("You win if you can guess the code in " + maxTurns + " turns or less!")
 		println
-		println("In order to help you out, you will have four hint markers.")
-		println("After every turn, you will be given the four markers, which help indicate if your choices are correct.")
+		println("In order to help you out, you will have " + numPins + " hint markers.")
+		println("After every turn, you will be given the " + numPins + " markers, which help indicate if your choices are correct.")
 		println("The white marker is represented by `-`. It means you have a correct pin, but it is in the wrong position.")
 		println("The black marker is represented by `o`. It means you have the correct pin, in the correct position.")
 		println("The empty marker is represented by `x`. It means you have an incorrect pin.")
@@ -86,7 +88,10 @@ object Mastermind {
 			response = input.nextLine
 			println("\n\n")
 		} while (!response.equals("") && !(response.equals("y") || response.equals("n") || response.equals("q")))
-		if (response.equals("n") || response.equals("q")) System.exit(1)
+
+		if (response.equals("n") || response.equals("q")) {
+			System.exit(1)
+		}
 	}
 
 	/**
@@ -106,6 +111,7 @@ object Mastermind {
 			turn += 1
 			println("\n\n")
 		}
+
 		if (win) {
 			println("Congratulations! You won!")
 		} else {
@@ -137,13 +143,16 @@ object Mastermind {
 	*/
 	def printBoard(): Unit = {
 		println("\nBoard:\n")
+
 		for (i <- turn to 0 by -1) {
 			var display = ""
-			for (pin <- guesses(i))
+			for (pin <- guesses(i)) {
 				display += pin
+			}
 			display += ",\t\t"
-			for (marker <- markers(i))
+			for (marker <- markers(i)) {
 				display += marker
+			}
 			println(display)
 		}
 	}
@@ -154,8 +163,10 @@ object Mastermind {
 	*	Selects the computer's pins and alerts the player when completed.
 	*/
 	def selectComputerPins(): Unit = {
-		for (i <- 0 until computerCode.length)
+		for (i <- 0 until computerCode.length) {
 			computerCode(i) = colorChoices((math.random * colorChoices.length).asInstanceOf[Int])
+		}
+
 		println("Computer has selected its pins.\n")
 		println(computerCode.mkString)
 	}
@@ -169,26 +180,34 @@ object Mastermind {
 	*/
 	def promptGuess(): Array[Char] = {
 		var guess = ""
+
 		do {
 			println("Select from: [`R`, `G`, `B`, `O`, `P`, `Y`]")
 			print("Enter a valid guess (e.g. `ROPY`)\t> ")
 			guess = input.nextLine
 		} while (!guessIsValid(guess))
+
 		return stripGuess(guess)
 	}
 
 	/**
 	*	guessIsValid
 	*
-	*	Determines whether a given guess is of 4 valid pins.
+	*	Determines whether a given guess is of valid pins.
 	*
 	*	param: (String) player's guess
 	*	return: (Boolean) whether player's guess is valid
 	*/
 	def guessIsValid(guess: String = null): Boolean = {
-		if (guess == null) return false
+		if (guess == null) {
+			return false
+		}
+
 		val trimmedGuess: Array[Char] = stripGuess(guess)
-		if (trimmedGuess.length != 4) return false
+		if (trimmedGuess.length != numPins) {
+			return false
+		}
+
 		return true
 	}
 
@@ -200,7 +219,7 @@ object Mastermind {
 	*	param: (String) player's guess
 	*	return: (Array[Char]) all valid pins player's guess
 	*/
-	def stripGuess(guess: String = null): Array[Char] = return guess.toUpperCase.toCharArray.filter(colorChoices.contains(_))
+	def stripGuess(guess: String = null): Array[Char] = guess.toUpperCase.toCharArray.filter(colorChoices.contains(_))
 
 	/**
 	*	checkGuess
@@ -214,76 +233,44 @@ object Mastermind {
 			gameOver = true
 			win = true
 		}
-		return determineMarkers
+
+		return configureHints
 	}
 
 	/**
-	*	determineMarkers
+	*	configureHints
 	*
-	*	Determines markers associated with a player's guess.
+	*	Determines hints associated with a player's guess.
 	*
 	*	return: (Array[Char]) markers associated with a guess
 	*/
-	def determineMarkers(): Array[Char] = {
-		val markers = new Array[Char](guesses(turn).length)
-		val matches = new ArrayBuffer[Int]
-		for (i <- 0 until guesses(turn).length)
+	def configureHints(): Array[Char] = {
+		val hints = new ArrayBuffer[Char]
+		val matched = new ArrayBuffer[Int]
+
+		for (i <- 0 until guesses(turn).length) {
 			if (guesses(turn)(i) == computerCode(i)) {
-				markers(i) = 'X'
-				matches += i
+				hints += 'o'
+				matched += i
 			}
-		for (i <- (0 until guesses(turn).length).filterNot(matches.contains(_))) {
+		}
+
+		for (i <- 0 until guesses(turn).length) {
 			for (k <- 0 until computerCode.length) {
-				if (guesses(turn)(i) == computerCode(k)) {
-					markers(i) = 'W'
-					matches += i
+				if (!matched.contains(i)) {
+					if (guesses(turn)(i) == computerCode(k)) {
+						hints += '-'
+						matched += i
+					}
 				}
 			}
 		}
-		return markersSort(markers)
-	}
 
-	/**
-	*	determineMarker
-	*
-	*	Determines the appropriate marker for a pin in a player's guess.
-	*
-	*	param: (Int) index of considered pin
-	*	return: (Char) marker for a pin
-	*/
-	def determineMarker(i: Int, matched: Array[Int]): Char = {
-		if (guesses(turn)(i) == computerCode(i))
-			return 'X'
-		for (k <- 0 until computerCode.length)
-			if (guesses(turn)(i) == computerCode(k) && !matched.contains(k)) {
-				return 'W'
-			}
-		return 'E'
-	}
-
-	/**
-	*	markersSort
-	*
-	*	Sorts the markers determined by accuracy.
-	*
-	*	param: (ArrayBuffer[Char]) unranked set of markers
-	*	return: (Array[Char]) markers ranked from most accurate to least
-	*/
-	def markersSort(markers: Array[Char]): Array[Char] = {
-		val buffer = new ArrayBuffer[Char]
-		for (i <- 0 until markers.length) {
-			if (markers(i) == 'X')
-				buffer.insert(0, 'X')
-			else if (markers(i) == 'W')
-				buffer.insert(buffer.lastIndexOf('X') + 1, 'W')
-			else
-				buffer += 'E'
+		val remaining = numPins - hints.length
+		for (i <- 0 until remaining) {
+			hints += 'x'
 		}
-		if (buffer.length > guesses(turn).length)
-			buffer.trimEnd(buffer.length - guesses(turn).length)
-		else if (buffer.length < guesses(turn).length)
-			for (i <- 0 until (guesses(turn).length - buffer.length))
-				buffer += 'E'
-		return buffer.toArray
+
+		return hints.toArray
 	}
 }
